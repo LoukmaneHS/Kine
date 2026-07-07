76db2dd77ce86664530256272346e4548cb825f8
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-07 10:47:37 +0100

    Replace Kimage import with create_motion

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from .core.classes.karacter import Karacter
 from .core.classes.kinematrix import Kinematrix
 from .core.classes.kimage import Kimage
-from .core.functions.motion import Kimage
+from .core.functions.motion import create_motion
