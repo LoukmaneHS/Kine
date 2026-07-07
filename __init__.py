06c75e866d1fcebcd470da6a15e2c6910db4b20c
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-07 11:40:21 +0100

    Refactor imports in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from .core.classes.karacter import Karacter
-from .core.classes.kinematrix import Kinematrix
+from .core.classes.kmatrix import Kmatrix
 from .core.classes.kimage import Kimage
+from .core.classes.kframe import Kframe
+
 from .core.functions.motion import create_motion
-from .core.functions.image import encode_image
-from .core.functions.image import decode_image
+from .core.functions.image import encode_image, decode_image
+from .core.functions.frame import create_frame, matframe, imgframe
